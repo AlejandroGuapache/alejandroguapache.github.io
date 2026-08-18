a5151d4c2078bf1e9855467ce554764e3d4fbad7
Alejandro G <jaguapache@viewnext.com>
2026-08-18 17:25:43 +0200

    feat: add Music section component with Spotify playlist link and animation

--- src/components/Sections/Music/index.vue
+++ src/components/Sections/Music/index.vue
@@ -25,7 +25,7 @@
 
     <a
       class="bg-brown px-7 py-2 rounded-3xl flex items-center mt-5 mb-4"
-      href="https://open.spotify.com/playlist/50PTAWep6JlFFaZgZC3T8k?si=ja9w8VMNQ6i34ZAcx55vfw&pi=VUiW_bnqQuqbk&pt=b3937e4c65da85aac45e18e93e616a81"
+      href="https://open.spotify.com/playlist/50PTAWep6JlFFaZgZC3T8k?si=agVWmiEAQMuSUTYARcYz6A&utm_source=whatsapp&pt=868e264f16fae4d6d6efa693a641c038&pi=pZn50_GSSTKjM"
       target="_blank"
     >
       <span class="text-white font-lora text-lg"> Agregar canciones </span>
